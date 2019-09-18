04ca815abaf4a09c4e388ea615e445cb4afdc2e0
Ellie <779537278@qq.com>
2019-09-18 20:37:29 +0800

    readme edit classify

--- src/components/sub/discover/Classify.vue
+++ src/components/sub/discover/Classify.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <div class="my-chosen" v-for="(item,index) in list" :key="index">
+  <div class="rootstyle">
+    <div class="my-chosen" v-for="(item,index) of opts" :key="index">
       <a href="">
-        <img src="" alt="">
-        <p></p>
+        <img :src="item.imgurl" alt="">
+        <p>{{item.name}}</p>
       </a>
     </div>
   </div>
@@ -13,17 +13,46 @@
 export default {
   data(){return {}},
   props:{
-    list:[
-      {imgurl:"@/assets/m_day.png",name:"每日推荐"},
-      {imgurl:"@/assets/m_songs.png",name:"歌单"},
-      {imgurl:"@/assets/m_charts.png",name:"排行榜"},
-      {imgurl:"@/assets/m_radio.png",name:"电台"},
-      {imgurl:"@/assets/m_broadcast.png",name:"直播"},
-    ]
-  }
+    opts:{default:""}
+    // opts:{
+    //   type:Array,
+    //   default:function(){
+    //     return [
+    //     // {imgurl:require("@/assets/m_day.png"),name:"每日推荐"},
+    //     // {imgurl:require("@/assets/m_songs.png"),name:"歌单"},
+    //     // {imgurl:require("@/assets/m_charts.png"),name:"排行榜"},
+    //     // {imgurl:require("@/assets/m_radio.png"),name:"电台"},
+    //     // {imgurl:require("@/assets/m_broadcast.png"),name:"直播"},
+    //     ]
+    //   }
+    // },
+  },
+  
 }
 </script>
 
-<style>
-
+<style scoped>
+  .rootstyle{
+    width:100%;
+    height:65px;
+    display:flex;
+    justify-content: space-around;
+  }
+  a{
+    display:line-block;
+    width:100%;
+    height:65px;
+    text-decoration-line:none;
+    text-align:center;
+  }
+  img{
+    width:50px;
+    height:50px;
+  }
+  p{
+    font-size:10px;
+    color:#333;
+    margin:-8px;
+    top:0;
+  }
 </style>
