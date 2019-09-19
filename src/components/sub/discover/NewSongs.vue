47f0b209646cc4b4c4296f01721181d87bd22a7f
Ellie <779537278@qq.com>
2019-09-19 20:47:52 +0800

    readme edit newsongs

--- src/components/sub/discover/NewSongs.vue
+++ src/components/sub/discover/NewSongs.vue
@@ -3,12 +3,20 @@
     <!-- 父面板 -->
     <mt-tab-container v-mdel="active">
       <!-- 子面板1 -->
-      <mt-tab-container-item id="">
-
+      <mt-tab-container-item id="newDiscs">
+        <div v-for="(item,index) of firstList" :key="index">
+          <img :src="item.imgurl" alt="">
+          <p>{{item.sname}}</p>
+          <p>{{item.name}}</p>
+        </div>
       </mt-tab-container-item>
       <!-- 子面板2 -->
-      <mt-tab-container-item id="">
-
+      <mt-tab-container-item id="newSongs">
+        <div v-for="(item,index) of secondList" :key="index">
+          <img :src="item.imgUrl" alt="">
+          <p>{{item.sname}}</p>
+          <p>{{item.name}}</p>
+        </div>
       </mt-tab-container-item>
     </mt-tab-container>
   </div>
@@ -16,7 +24,15 @@
 
 <script>
 export default {
-
+  data(){
+    return {
+      active:"newDiscs",
+    }
+  },
+  props:{
+    firstList:{default:""},
+    secondList:{default:""},
+  }
 }
 </script>
 
